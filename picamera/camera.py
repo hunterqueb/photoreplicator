487da0fc611ee4c60263c52641b96b2dc8125ff1
Hunter Quebedeaux <hunterq@knights.ucf.edu>
2020-11-18 23:26:18 -0500

    tested picamera and everything works

--- picamera/camera.py
+++ picamera/camera.py
@@ -3,3 +3,7 @@ from picamera import PiCamera
 
 # initialize the camera object
 camera = PiCamera()
+camera.start_preview(alpha=200)
+sleep(1)
+camera.capture('/home/pi/Desktop/photoreplicator/picamera/photos/image.jpg')
+camera.stop_preview()
