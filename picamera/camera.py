76245cf24e973bc3b110ebf2af8e9e200b569995
Hunter Quebedeaux <hunterq@knights.ucf.edu>
2020-11-19 14:16:59 -0500

    tested image quality of camera module

--- picamera/camera.py
+++ picamera/camera.py
@@ -3,7 +3,12 @@ from picamera import PiCamera
 
 # initialize the camera object
 camera = PiCamera()
+camera.resolution = (1920, 1080) # max res is 2592x1944 for still images, 1920x1080 for vidoes.
+camera.framerate = 24
+camera.rotation = 180
 camera.start_preview(alpha=200)
 sleep(1)
-camera.capture('/home/pi/Desktop/photoreplicator/picamera/photos/image.jpg')
+for i in range(2):
+    camera.capture('/home/pi/Desktop/photoreplicator/picamera/photos/image%s.jpg' % str(i+1))
+    camera.resolution = (2592, 1944)
 camera.stop_preview()
