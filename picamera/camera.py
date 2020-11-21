aebb03c5b865f6a1ef9aa6ebb759411ee5964454
Hunter Quebedeaux <hunterq@knights.ucf.edu>
2020-11-21 03:21:52 -0500

    tested funcitonality for class implemention for capturing camera module

--- picamera/camera.py
+++ picamera/camera.py
@@ -9,30 +9,24 @@ class CameraModule:
         self.camera.framerate = frameRate
         self.camera.rotation = rot
         self.filePath = os.path.dirname(os.path.abspath(__file__))
-        self.count = 0
-        for path in os.listdir(self.filePath):
-            if os.path.isfile(os.path.join(self.filePath, path)):
-                self.count += 1
+        self.photosPath = self.filePath + "/photos/"
+        path,dirs,files = next(os.walk(self.photosPath))
+        self.photoCount = len(files)
 
     def takePic(self):
-        camera.start_preview(alpha=200)
-        self.camera.capture(self.filePath + '/photos/image%s.jpg' % str(self.count))
-        sleep(3)
-        camera.stop_preview()
+        self.camera.start_preview(alpha=200)
+        self.camera.capture(self.filePath + '/photos/image%s.jpg' % str(self.photoCount))
+        sleep(1)
+        self.camera.stop_preview()
+        self.photoCount = self.photoCount + 1
 
+width = 1920
+height = 1080
+framerate = 24
+rotation = 180
+
+
+CAM = CameraModule(width,height,framerate,rotation)
+
+CAM.takePic()
     
-
-
-# filePath = os.path.dirname(os.path.abspath(__file__))
-# print(filePath)
-# # initialize the camera object
-# camera = PiCamera()
-# camera.resolution = (1920, 1080) # max res is 2592x1944 for still images, 1920x1080 for vidoes.
-# camera.framerate = 24
-# camera.rotation = 180
-# camera.start_preview(alpha=200)
-# sleep(1)
-# for i in range(2):
-#     camera.capture(filePath + '/photos/image%s.jpg' % str(i+1))
-#     camera.resolution = (2592, 1944)
-# camera.stop_preview()
