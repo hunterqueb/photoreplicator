fc8ecd28aadba4179bdcc11074b82bfc0dafb4eb
hunterq55 <quebedeauxh55@gmail.com>
2020-12-23 21:50:28 -0500

    added function to generate an RGB value from the wavelength in nm and a tester file

---
+++ stl-voxel/wavelengtToRGB/tester.py
@@ -0,0 +1,11 @@
+
+from wavelengthToRGB import wavelengthToRGB
+
+
+[r1, g1, b1] = wavelengthToRGB(450, 0.8)
+[r2, g2, b2] = wavelengthToRGB(450, 1)
+[r3, g3, b3] = wavelengthToRGB(450, 0.5)
+
+print([r1, g1, b1])
+print([r2, g2, b2])
+print([r3, g3, b3])
