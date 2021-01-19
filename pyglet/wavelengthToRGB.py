5a81299e47f35393d94a10b5be6f902dee7a7fa1
Hunter Quebedeaux <hunterq@knights.ucf.edu>
2021-01-19 17:16:06 -0500

    moved wavelengthtoRGB function for fasting development

---
+++ pyglet/wavelengthToRGB.py
@@ -0,0 +1,62 @@
+'''
+    == A few notes about color ==
+
+    Color   Wavelength(nm) Frequency(THz)
+    Red     620-750        484-400
+    Orange  590-620        508-484
+    Yellow  570-590        526-508
+    Green   495-570        606-526
+    Blue    450-495        668-606
+    Violet  380-450        789-668
+'''
+
+
+def wavelengthToRGB(wavelength, gamma):
+    '''This converts a given wavelength of light to an
+    approximate RGB color value. The wavelength must be given
+    in nanometers in the range from 380 nm through 750 nm
+    (789 THz through 400 THz). Gamma is used to determine the brightness
+    of the color
+    
+    Original code from 
+    http://www.noah.org/wiki/wavelengthToRGB_in_Python
+
+    Based on code by Dan Bruton
+    http://www.physics.sfasu.edu/astro/color/spectra.html
+    '''
+
+    wavelength = float(wavelength)
+    if wavelength >= 380 and wavelength <= 440:
+        attenuation = 0.3 + 0.7 * (wavelength - 380) / (440 - 380)
+        R = ((-(wavelength - 440) / (440 - 380)) * attenuation) ** gamma
+        G = 0.0
+        B = (1.0 * attenuation) ** gamma
+    elif wavelength >= 440 and wavelength <= 490:
+        R = 0.0
+        G = ((wavelength - 440) / (490 - 440)) ** gamma
+        B = 1.0
+    elif wavelength >= 490 and wavelength <= 510:
+        R = 0.0
+        G = 1.0
+        B = (-(wavelength - 510) / (510 - 490)) ** gamma
+    elif wavelength >= 510 and wavelength <= 580:
+        R = ((wavelength - 510) / (580 - 510)) ** gamma
+        G = 1.0
+        B = 0.0
+    elif wavelength >= 580 and wavelength <= 645:
+        R = 1.0
+        G = (-(wavelength - 645) / (645 - 580)) ** gamma
+        B = 0.0
+    elif wavelength >= 645 and wavelength <= 750:
+        attenuation = 0.3 + 0.7 * (750 - wavelength) / (750 - 645)
+        R = (1.0 * attenuation) ** gamma
+        G = 0.0
+        B = 0.0
+    else:
+        R = 0.0
+        G = 0.0
+        B = 0.0
+    R *= 255
+    G *= 255
+    B *= 255
+    return (int(R), int(G), int(B))
