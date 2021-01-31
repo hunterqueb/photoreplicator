c0a549951a5f05875647989edfa4f850c4bfebb5
Hunter Quebedeaux <hunterq@knights.ucf.edu>
2021-01-31 14:47:16 -0500

    removed references to second montior, wont run if there is only one monitor -- need to add exception handling for that

--- Projections/solidRectangle.py
+++ Projections/solidRectangle.py
@@ -17,15 +17,15 @@ screens = displays.get_screens()
 # instance the window object for each display
 window1 = pyglet.window.Window(
     fullscreen=True, screen=screens[0], visible=False)
-window2 = pyglet.window.Window(
-    fullscreen=True, screen=screens[1], visible=False)
+#window2 = pyglet.window.Window(
+#    fullscreen=True, screen=screens[1], visible=False)
 
 # same text
-label = pyglet.text.Label('Hello, world',
-                          font_name='Times New Roman',
-                          font_size=36,
-                          x=window2.width//2, y=window2.height//2,
-                          anchor_x='center', anchor_y='center')
+#label = pyglet.text.Label('Hello, world',
+#                          font_name='Times New Roman',
+#                          font_size=36,
+#                          x=window2.width//2, y=window2.height//2,
+#                          anchor_x='center', anchor_y='center')
 
 # create a batch of shapes the program draws
 batch = pyglet.graphics.Batch()
@@ -52,10 +52,10 @@ def on_draw():
     batch.draw()
     
 
-@window2.event
-def on_draw():
-    window2.clear()
-    label.draw()
+#@window2.event
+#def on_draw():
+#    window2.clear()
+#    label.draw()
 
 @window1.event
 def on_key_press(symbol, modifiers):
