dd7687a5fb9bbc048c667db2f23ec90d5be8e2e1
Hunter Quebedeaux <hunterq@knights.ucf.edu>
2021-02-03 20:57:42 -0500

    Added comments and renamed file

---
+++ Projections/stepperDriverTest.py
@@ -0,0 +1,40 @@
+import RPi.GPIO as GPIO
+import time
+
+# some notes on stepper motors
+# the pi outputs a high signal from the GPIO pins as 3.3v and not 5v like the arduino, this means that certain drivers, like the one we are using
+# will not work out of the box, we need to connect a 3.3v to 5v circuit to get the required power.
+# 
+# wire connections
+# 
+# GPIO 12 to PUL on driver
+# GPIO 11 to DIR on driver
+#  
+# 
+# 
+# 
+
+
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+
+PUL = 12 # needs to be on a pwn pin - pwn pins are 12, 32, 33, 35 
+DIR = 11
+
+# OPTO and ENA pins are not needed for this application
+
+GPIO.setup(DIR, GPIO.out)
+pwmPin = GPIO.PWM(PUL, 133333) # 2nd arg. pulse width in hz, minimum for our driver is 7.5 microseconds or 1/(7.5 microsec)
+pwmPin.start(0)  # start the pwm at 0
+
+SPR = 400 # value dependent on the switches on the outside of the driver
+
+pwmPin.ChangeDutyCycle(50) # half power - this value has a range of 0 to 100 to determine the speed of the motor
+time.sleep(5) 
+pwmPin.ChangeDutyCycle(100)
+time.sleep(5)
+pwmPin.ChangeDutyCycle(20)
+time.sleep(5)
+
+GPIO.cleanup()
