b25bf0ae6e378815fe6387b0473e0a3b61dfc796
Hunter Quebedeaux <hunterq@knights.ucf.edu>
2021-02-17 00:22:09 -0500

    Rearranged libraries and deleted unused files

--- Projections/libraries/stepperMotor/StepperMotorClass.py
+++ Projections/libraries/stepperMotor/StepperMotorClass.py
@@ -17,6 +17,11 @@ class StepperMotor:
         self.OPTO = OPTO
         self.LEAD = LEAD
         self.PULSES_PER_REV = PULSES_PER_REV
+        self.LEAD_DISTANCE = 8 # in mm
+        self.PULSES_PER_SEC = None
+        self.PULSE_DELAY = None
+        self.STEPS_PER_REV = 200 # same for both nema 11 and nema 17
+
 
         GPIO.setup(self.PUL, GPIO.OUT)
         GPIO.setup(self.OPTO, GPIO.OUT)
@@ -50,15 +55,33 @@ class StepperMotor:
             GPIO.output(self.PUL, GPIO.LOW)
             sleep(self.PULSE_DELAY)
 
+    def driveMotorLead(self,distance,TIME_OF_ROTATION):
+        if self.LEAD == 0:
+            print("Error! This motor is not configured as a lead screw motor. As a result, you cannot use this command. Recreate the stepper motor object!")
+            return
+        
+        self.PULSES_PER_SEC = 2 * self.PULSES_PER_REV * distance / (self.LEAD_DISTANCE * TIME_OF_ROTATION)
+        self.PULSE_DELAY = 0.5 * 1 / self.PULSES_PER_SEC
+
+        for x in range(self.PULSES_PER_SEC):
+            GPIO.output(self.PUL, GPIO.HIGH)
+            sleep(self.PULSE_DELAY)
+            GPIO.output(self.PUL, GPIO.LOW)
+            sleep(self.PULSE_DELAY)
+
     def changeDirection(self):
         if self.currentDirection == GPIO.LOW:
             self.currentDirection = GPIO.HIGH
         else:
             self.currentDirection = GPIO.LOW
 
-
         GPIO.output(self.OPTO, GPIO.LOW)
         sleep(0.001)
         GPIO.output(self.DIR, self.currentDirection)
         GPIO.output(self.OPTO, GPIO.HIGH)
 
+    def changeLead(self):
+        if self.LEAD == 0:
+            self.LEAD = 1
+        else:
+            self.LEAD = 0
