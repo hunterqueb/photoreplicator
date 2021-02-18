4428e275cd6459894d63869a23c603feaddb38b8
Hunter Quebedeaux <hunterq@knights.ucf.edu>
2021-02-18 14:29:56 -0500

    added ghost pins

--- Projections/motorClassTest.py
+++ Projections/motorClassTest.py
@@ -1,9 +1,9 @@
 from multipleMotorClass import StepperMotors
 
-VOLT = [2] # tester to see if pi pinout can handle lvl converting using GPIO pins
-PUL = [17]  # Stepper Drive Pulses
-DIR = [27]  # Controller Direction Bit (High for Controller default / LOW to Force a Direction Change).
-OPTO = [22]  # Controller Enable Bit (High to Enable / LOW to Disable).
+VOLT = [2,21,20] # tester to see if pi pinout can handle lvl converting using GPIO pins
+PUL = [17,26,16]  # Stepper Drive Pulses
+DIR = [27,19,13]  # Controller Direction Bit (High for Controller default / LOW to Force a Direction Change).
+OPTO = [22,6,12]  # Controller Enable Bit (High to Enable / LOW to Disable).
 
 stepper1 = StepperMotors(VOLT,PUL,DIR,OPTO,400,0,1)
 
