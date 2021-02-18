aac13d03c6e11d4d771233a9646ddfe025a96213
Hunter Quebedeaux <hunterq@knights.ucf.edu>
2021-02-18 15:40:48 -0500

    updated movement and added pawn figure

--- Projections/generateShape.py
+++ Projections/generateShape.py
@@ -22,9 +22,9 @@ displays = pyglet.canvas.get_display()
 screens = displays.get_screens()
 
 # instance the window object for each display
-window1 = pyglet.window.Window(1920, 1080, fullscreen=False, screen=screens[1], visible=True)
+window1 = pyglet.window.Window(1920, 1080, fullscreen=False, screen=screens[0], visible=True)
 #window2 = pyglet.window.Window(
-#    fullscreen=True, screen=screens[1], visible=False)
+#    fullscreen=True, screen=screens[0], visible=False)
 
 
 # create a batch of shapes the program draws
@@ -44,23 +44,36 @@ colorToDraw = visibleForegroundWavelenth
 
 # create variables that store the value of where the shapes should be drawn
 background = shapes.Rectangle(
-    0, 0, screens[1].width, screens[1].height, color=wavelengthToRGB(backgroundWavelength, gamma), batch=backgroundBatch)
+    0, 0, screens[0].width, screens[0].height, color=wavelengthToRGB(backgroundWavelength, gamma), batch=backgroundBatch)
 
-purpleBackground = shapes.Rectangle(0, 0, screens[1].width, screens[1].height, color=wavelengthToRGB(
+purpleBackground = shapes.Rectangle(0, 0, screens[0].width, screens[0].height, color=wavelengthToRGB(
     foregroundWavelength, gamma), batch=resinPreheat)
 
-yellowBackground = shapes.Rectangle(0, 0, screens[1].width, screens[1].height, color=wavelengthToRGB(
+yellowBackground = shapes.Rectangle(0, 0, screens[0].width, screens[0].height, color=wavelengthToRGB(
     backgroundWavelength, gamma), batch=deactivateResin)
 
+foregroundObject = [None,None,None]
 
 if objectToProject == "rectangle":
     rectangleWidth = 200
     rectangleHeight = 600
-    foregroundObject = shapes.Rectangle(
-        width=rectangleWidth, height=rectangleHeight, x=screens[1].width//2 - rectangleWidth, y=screens[1].height//2 - rectangleHeight, color=wavelengthToRGB(colorToDraw, gamma), batch=batch)
+    foregroundObject[0] = shapes.Rectangle(
+        width=rectangleWidth, height=rectangleHeight, x=screens[0].width//2 - rectangleWidth, y=screens[0].height//2 - rectangleHeight, color=wavelengthToRGB(colorToDraw, gamma), batch=batch)
 elif objectToProject == "circle":
-    foregroundObject = shapes.Circle(
-        x=screens[1].width//2, y=screens[1].height//2, radius=50, color=wavelengthToRGB(colorToDraw, gamma), batch=batch)
+    foregroundObject[0] = shapes.Circle(
+        x=screens[0].width//2, y=screens[0].height//2, radius=250, color=wavelengthToRGB(colorToDraw, gamma), batch=batch)
+elif objectToProject == "pawn":
+    rectangleWidth = 200
+    rectangleHeight = 600
+    foregroundObject[0] = shapes.Rectangle(
+        width=rectangleWidth, height=rectangleHeight/2, x=screens[0].width//2, y=screens[0].height//2 - rectangleHeight, color=wavelengthToRGB(colorToDraw, gamma), batch=batch)
+    foregroundObject[1] = shapes.Circle(
+        x=screens[0].width//2 + rectangleWidth//2, y=screens[0].height//3, radius=250, color=wavelengthToRGB(colorToDraw, gamma), batch=batch)
+    foregroundObject[2] = shapes.Rectangle(
+        width=rectangleHeight, height=rectangleWidth/2, x=screens[0].width//2 - rectangleWidth, y=screens[0].height//2 - rectangleHeight, color=wavelengthToRGB(colorToDraw, gamma), batch=batch)
+
+
+
 draw = 0
 
 
@@ -95,14 +108,34 @@ def on_key_press(symbol, modifiers):
     if symbol == key.D:
         draw = 2
     if symbol == key.LEFT:
-        foregroundObject.x -= 10
+        try:
+            foregroundObject[0].x -= 10
+            foregroundObject[1].x -= 10
+            foregroundObject[2].x -= 10
+        except:
+            pass
     if symbol == key.RIGHT:
-        foregroundObject.x += 10
-    if symbol == key.UP:
-        foregroundObject.y += 10
-    if symbol == key.DOWN:
-        foregroundObject.y -= 10
+        try:
+            foregroundObject[0].x += 10
+            foregroundObject[1].x += 10
+            foregroundObject[2].x += 10
+        except:
+            pass
 
+    if symbol == key.UP:
+        try:
+            foregroundObject[0].y += 10
+            foregroundObject[1].y += 10
+            foregroundObject[2].y += 10
+        except:
+            pass
+    if symbol == key.DOWN:
+        try:
+            foregroundObject[0].y -= 10
+            foregroundObject[1].y -= 10
+            foregroundObject[2].y -= 10
+        except:
+            pass
 
 def drawBatch(screenToDraw):
     backgroundBatch.draw()
