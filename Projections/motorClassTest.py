cc87a8d6ce2d745aed91e74adf155d8c3597b77a
Hunter Quebedeaux <hunterq@knights.ucf.edu>
2021-02-23 12:59:34 -0500

    added constants for pulses per rev

--- Projections/motorClassTest.py
+++ Projections/motorClassTest.py
@@ -5,6 +5,12 @@ PUL = [17,26,16]  # Stepper Drive Pulses
 DIR = [27,19,13]  # Controller Direction Bit (High for Controller default / LOW to Force a Direction Change).
 OPTO = [22,6,12]  # Controller Enable Bit (High to Enable / LOW to Disable).
 
-stepper1 = StepperMotors(VOLT,PUL,DIR,OPTO,[400],0,1)
+PULSES_PER_REV_MOTOR1 = 400
+PULSES_PER_REV_MOTOR2 = 400
+PULSES_PER_REV_MOTOR3 = 400
+
+PULSES_PER_REV = [PULSES_PER_REV_MOTOR1,PULSES_PER_REV_MOTOR2,PULSES_PER_REV_MOTOR3]
+
+stepper1 = StepperMotors(VOLT,PUL,DIR,OPTO,PULSES_PER_REV,0,1)
 
 stepper1.driveRotMotor(1,1)
