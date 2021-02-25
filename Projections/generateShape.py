a987936f1fb0bfcd7fd6126339aa17df256ef421
Hunter Quebedeaux <hunterq@knights.ucf.edu>
2021-02-24 23:44:46 -0500

    added comments to the main generateShape script

--- Projections/generateShape.py
+++ Projections/generateShape.py
@@ -7,7 +7,6 @@ from libraries.wavelengthToRGB.wavelengthToRGB import wavelengthToRGB
 from libraries.vertexClass.vertexClass import pygletVertex
 
 # TODO
-# add cup -- u shaped object
 # fix pawn shape
 # add hourglass shape
 # look into import shapes/sillouettes into pyglet
@@ -16,7 +15,10 @@ from libraries.vertexClass.vertexClass import pygletVertex
 
 # prints 30 pixels/mm
 # accuracy of 33 microns
+# this is subject to change as a result of changing the focul lengths of the magnifying glasses
 
+# # # PREWINDOW SETUP # # #
+# taking the input from the user, we need to know what shape to draw. if no input is added, then we default to a rectangle
 try:
     objectToProject = sys.argv[1]
 except:
@@ -32,23 +34,27 @@ screens = displays.get_screens()
 window1 = pyglet.window.Window(1920, 1080, fullscreen=False, screen=screens[0], visible=False)
 # window2 = pyglet.window.Window(1920, 1080, fullscreen=False, screen=screens[1], visible=False)
 
+# # # PREDRAWING SETUP # # #
 
-# create a batch of shapes the program draws
+# create a batch of shapes the program draws - this is how pyglet draws. you load things you want to project into a batch, then when drawing the batch it draws all shapes at once
 resinPreheat = pyglet.graphics.Batch()
 deactivateResin = pyglet.graphics.Batch()
 backgroundBatch = pyglet.graphics.Batch()
 
+# this batch is a seperate one for drawing the foreground object
 batch = pyglet.graphics.Batch()
+
 # initialize the wavelengths for colors to be used by the wavelength to RGB function
 gamma = 1
 #colors in wavelengths in nanometers
 backgroundWavelength = 580  # yellow
 foregroundWavelength = 405  # purple
 visibleForegroundWavelenth = 680 # red
-
+# initialize all shapes that are first drawn to the visible foreground wavelength to help with lining up the image in the buid volume
 colorToDraw = visibleForegroundWavelenth
 
-# create variables that store the value of where the shapes should be drawn
+# create variables that store the value of where the shapes should be drawn -- this starts with the backgrounds
+# 3 backgrounds are generated, one for normal use as the standard background, one that is for preactivating the resin, and one for having no image show up 
 background = shapes.Rectangle(
     0, 0, screens[0].width, screens[0].height, color=wavelengthToRGB(backgroundWavelength, gamma), batch=backgroundBatch)
 
@@ -58,19 +64,24 @@ purpleBackground = shapes.Rectangle(0, 0, screens[0].width, screens[0].height, c
 yellowBackground = shapes.Rectangle(0, 0, screens[0].width, screens[0].height, color=wavelengthToRGB(
     backgroundWavelength, gamma), batch=deactivateResin)
 
-NUM_OF_POLYGONS = 5
 
+# as well, some of the primitive shapes use pyglets framework, and we can draw more complex shapes using a variety of polygons. here, we initilze the 
+# foregroundObjectShapes array with the number of polygons
+NUM_OF_POLYGONS = 5
 foregroundObjectShapes = [None]*NUM_OF_POLYGONS
 
+# # # DRAWING THE OBEJECT TO PROJECT # # #
+
+# when drawing the object either the pyglet framework is used which is simpler to use and read, but cannot draw complex shapes very well
 if objectToProject == "rectangle":
     rectangleWidth = 500
     rectangleHeight = 120
     foregroundObjectShapes[0] = shapes.Rectangle(
         width=rectangleWidth, height=rectangleHeight, x=screens[0].width//2 - rectangleWidth, y=screens[0].height//2 - rectangleHeight, color=wavelengthToRGB(colorToDraw, gamma), batch=batch)
 elif objectToProject == "circle":
-    foregroundObjectShapes[0] = shapes.Circle(
-        x=screens[0].width//2, y=screens[0].height//2, radius=250, color=wavelengthToRGB(colorToDraw, gamma), batch=batch)
+    foregroundObjectShapes[0] = shapes.Circle(x=screens[0].width//2, y=screens[0].height//2, radius=250, color=wavelengthToRGB(colorToDraw, gamma), batch=batch)
 elif objectToProject == "pawn":
+    # our first primitive complex shape. this draws a psuedo pawn that looks sort of like a cartoon
     rectangleWidth = 200
     rectangleHeight = 600
     foregroundObjectShapes[0] = shapes.Rectangle(
@@ -84,9 +95,8 @@ elif objectToProject == "polygonTest":
     # first define the objectDrawn object using the class, pass in the batch, vertex count, and vertices locations
     # the idea is we can find test however we want.
     polygon = [100, 700, 300, 700, 300, 100, 100, 100]
-    polygonVertexNum = int(len(polygon)/2)
 
-    objectDrawn = pygletVertex(batch, polygonVertexNum, polygon)
+    objectDrawn = pygletVertex(batch, polygon)
     # now draw the initial batches
     vertexList = objectDrawn.initialDraw(batch)
     
@@ -108,8 +118,7 @@ elif objectToProject == "classTest":
     for i in range(len(polygon)):
         polygon[i] = int(polygon[i])
 
-    polygonVertexNum = int(len(polygon)/2)
-    objectDrawn = pygletVertex(batch, polygonVertexNum, polygon)
+    objectDrawn = pygletVertex(batch, polygon)
     vertexList = objectDrawn.initialDraw(batch)
     vertexList.delete()
     vertexList = objectDrawn.scalePolygon(batch,0.85)
@@ -125,7 +134,10 @@ elif objectToProject == "cup":
     foregroundObjectShapes[2] = shapes.Rectangle(
         width=rectangleWidth, height=rectangleHeight, x=screens[0].width//2, y=screens[0].height//2-400, color=wavelengthToRGB(colorToDraw, gamma), batch=batch)
 
+# this draw variable is used to tell the window what batch we want to draw. in this case, 0 indicates that we want the foreground object to be drawn immedietely
 draw = 0
+
+# # # EVENT HANDLING # # #
 
 # create event handlers that update with drawing the batch, im not sure how often this occurs
 @window1.event
@@ -138,14 +150,20 @@ def on_draw():
 #    window2.clear()
 #    label.draw()
 
+# another event handler to take in key presses
 @window1.event
 def on_key_press(symbol, modifiers):
+    # these gloab variables are used to modify the shapes drawn or location
     global draw
     global vertexList
+    # exit the window if either key is presses
     if symbol == key.ENTER or symbol == key.ESCAPE:
         pyglet.app.exit()
+    
+    # next sequence of keys are 1 and 2 which tell the window to edit the foreground objects color, 1 for red, 2 for violet
     if symbol == key._1:
         colorToDraw = visibleForegroundWavelenth
+        # these try and except tries to apply the changes to the either the foreground objects using pyglets framework or my custom class
         try:
             if foregroundObjectShapes[0] == None:
                 vertexList.delete()
@@ -167,7 +185,11 @@ def on_key_press(symbol, modifiers):
                     foregroundObjectShapes[i].color = wavelengthToRGB(colorToDraw, gamma)
         except:
             pass
+
+    # next sequence of keys are Z and X which tell the window to edit the size of the foreground objects
     if symbol == key.Z:
+        # increases the size of the objects, however, with the pyglet framework, when complex shapes are used, we can get strange results so it is disabled for those shapes
+        # however, it is enabled for the custom vertex class
         try:
             for i in range(len(foregroundObjectShapes)):
                 if objectToProject == "rectangle":
@@ -175,9 +197,13 @@ def on_key_press(symbol, modifiers):
                     foregroundObjectShapes[i].height = 1.01*foregroundObjectShapes[i].height
                 elif objectToProject == "circle":
                     foregroundObjectShapes[i].radius = 1.01 * foregroundObjectShapes[i].radius
+                else:
+                    vertexList.delete()
+                    vertexList = objectDrawn.scalePolygon(batch, 1.01)
         except:
             pass
     if symbol == key.X:
+        # decreases the size of the objects, see note above about complex shapes
         try:
             for i in range(len(foregroundObjectShapes)):
                 if objectToProject == "rectangle":
@@ -185,15 +211,22 @@ def on_key_press(symbol, modifiers):
                     foregroundObjectShapes[i].height = 0.99 * foregroundObjectShapes[i].height
                 elif objectToProject == "circle":
                     foregroundObjectShapes[i].radius = 0.99 * foregroundObjectShapes[i].radius
+                else:
+                    vertexList.delete()
+                    vertexList = objectDrawn.scalePolygon(batch, 0.99)
         except:
             pass
+    # next sequence of keys are A, S and D which tell the window which batch/background to draw
     if symbol == key.A:
-        draw = 0
+        draw = 0 # draw foregroundobject
     if symbol == key.S:
-        draw = 1
+        draw = 1  # draw preheatresin
     if symbol == key.D:
-        draw = 2
+        draw = 2  # draw just yellow background
+    
+    # next sequence of keys are LEFT,RIGHT,UP,DOWN which move the object around the window
     if symbol == key.LEFT:
+        # these try and except tries to apply the changes to the either the foreground objects using pyglets framework or my custom class
         try:
             if foregroundObjectShapes[0] == None:
                 vertexList.delete()
@@ -234,6 +267,7 @@ def on_key_press(symbol, modifiers):
         except:
             pass
 
+# this function gets the screen to draw and is called whenever the batch gets updated to draw the screen 
 def drawBatch(screenToDraw):
     backgroundBatch.draw()
     if screenToDraw == 0:
@@ -248,5 +282,7 @@ def drawBatch(screenToDraw):
 window1.set_visible()
 # window2.set_visible()
 
-# run the app
+# # # MAIN WINDOWLOOP # # #
+
+# run the app loop -- here in the future is where you would control the fps and do the manual window flip in a while loop
 pyglet.app.run()
