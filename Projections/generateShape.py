9cee129ac62d5a61779be7356104e148a98b3837
Hunter Quebedeaux <hunterq@knights.ucf.edu>
2021-03-04 14:30:34 -0500

    increased shape

--- Projections/generateShape.py
+++ Projections/generateShape.py
@@ -91,8 +91,8 @@ elif objectToProject == "pawn":
     foregroundObjectShapes[2] = shapes.Rectangle(
         width=rectangleHeight, height=rectangleWidth/2, x=screens[0].width//2 - rectangleWidth, y=screens[0].height//2 - rectangleHeight, color=wavelengthToRGB(colorToDraw, gamma), batch=batch)
 elif objectToProject == "polygonTest":
-    rectangleWidth = 220
-    rectangleHeight = 500
+    rectangleWidth = int(220*1.2)
+    rectangleHeight = int(500*1.2)
     startPos = 100
     # in order to draw a polygon, you need to follow this format
     # first define the objectDrawn object using the class, pass in the batch, vertex count, and vertices locations
